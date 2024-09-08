04c9bd8ba5e3aa15bd304afc16353fac63dd9ef0
Tahir Saifi <tahirhakim.52@me.com>
2024-09-08 14:52:22 +0400

    Initial Commit

---
+++ Appdemo/AppdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppdemoApp.swift
+//  Appdemo
+//
+//  Created by Tahir Saifi on 08/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppdemoTests/AppdemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  AppdemoTests.swift
+//  AppdemoTests
+//
+//  Created by Tahir Saifi on 08/09/2024.
+//
+
+import Testing
+
+struct AppdemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
